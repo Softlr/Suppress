285fcb1795294503bcb29fecaaf17efbb08d4d64
RaYell <lukasz@rajchel.pl>
2021-12-20 09:06:24 +0100

    file-scoped namespaces

--- src/Softlr.Suppress/Suppress.cs
+++ src/Softlr.Suppress/Suppress.cs
@@ -1,26 +1,25 @@
-namespace Softlr
-{
-    using System.Diagnostics.CodeAnalysis;
+namespace Softlr;
 
-    [ExcludeFromCodeCoverage]
-    [SuppressMessage(SONARQUBE, S2339)]
-    public static partial class Suppress
-    {
-        public const string CODE_CRACKER = "CodeCracker.CSharp";
-        public const string FXCOP = "FxCop";
-        public const string MICROSOFT_DESIGN = "Microsoft.Design";
-        public const string MICROSOFT_DOCUMENTATION = "Microsoft.Documentation";
-        public const string MICROSOFT_GLOBALIZATION = "Microsoft.Globalization";
-        public const string MICROSOFT_INTEROPERABILITY = "Microsoft.Interoperability";
-        public const string MICROSOFT_MAINTAINABILITY = "Microsoft.Maintainability";
-        public const string MICROSOFT_NAMING = "Microsoft.Naming";
-        public const string MICROSOFT_PERFORMANCE = "Microsoft.Performance";
-        public const string MICROSOFT_PUBLISH = "Microsoft.Publish";
-        public const string MICROSOFT_RELIABILITY = "Microsoft.Reliability";
-        public const string MICROSOFT_SECURITY = "Microsoft.Security";
-        public const string MICROSOFT_USAGE = "Microsoft.Usage";
-        public const string SONARQUBE = "SonarQube";
-        public const string STYLECOP = "StyleCop.CSharp";
-        private const string DROPPED_RULE = "This rule has been dropped.";
-    }
+using System.Diagnostics.CodeAnalysis;
+
+[ExcludeFromCodeCoverage]
+[SuppressMessage(SONARQUBE, S2339)]
+public static partial class Suppress
+{
+    public const string CODE_CRACKER = "CodeCracker.CSharp";
+    public const string FXCOP = "FxCop";
+    public const string MICROSOFT_DESIGN = "Microsoft.Design";
+    public const string MICROSOFT_DOCUMENTATION = "Microsoft.Documentation";
+    public const string MICROSOFT_GLOBALIZATION = "Microsoft.Globalization";
+    public const string MICROSOFT_INTEROPERABILITY = "Microsoft.Interoperability";
+    public const string MICROSOFT_MAINTAINABILITY = "Microsoft.Maintainability";
+    public const string MICROSOFT_NAMING = "Microsoft.Naming";
+    public const string MICROSOFT_PERFORMANCE = "Microsoft.Performance";
+    public const string MICROSOFT_PUBLISH = "Microsoft.Publish";
+    public const string MICROSOFT_RELIABILITY = "Microsoft.Reliability";
+    public const string MICROSOFT_SECURITY = "Microsoft.Security";
+    public const string MICROSOFT_USAGE = "Microsoft.Usage";
+    public const string SONARQUBE = "SonarQube";
+    public const string STYLECOP = "StyleCop.CSharp";
+    private const string DROPPED_RULE = "This rule has been dropped.";
 }
