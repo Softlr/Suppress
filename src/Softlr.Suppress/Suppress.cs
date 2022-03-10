5f0a82871a3bd02e2dc32cf368cd9b09b3b08700
RaYell <lukasz@rajchel.pl>
2022-03-10 22:03:47 +0100

    implicit usings

--- src/Softlr.Suppress/Suppress.cs
+++ src/Softlr.Suppress/Suppress.cs
@@ -1,7 +1,5 @@
 namespace Softlr;
 
-using System.Diagnostics.CodeAnalysis;
-
 [ExcludeFromCodeCoverage]
 [SuppressMessage(SONARQUBE, S2339)]
 public static partial class Suppress
--- test/Softlr.Suppress.Tests/GlobalUsings.cs
+++ test/Softlr.Suppress.Tests/GlobalUsings.cs
@@ -1,6 +1,4 @@
-global using System.Collections.Generic;
 global using System.Diagnostics.CodeAnalysis;
-global using System.Linq;
 global using FluentAssertions;
 global using Xunit;
 global using static System.Reflection.BindingFlags;
